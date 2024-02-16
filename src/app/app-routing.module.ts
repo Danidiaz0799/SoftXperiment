a1bb569de56e98861049c54a503fc7c4ab5df3bd
Steven Diaz <steven.diaz@amaris.com>
2024-02-16 02:00:50 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRoutes } from './app.routes';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(AppRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import {Routes} from '@angular/router';
+import { HomeComponent } from './global/components/home/home.component';
+
+
+export const AppRoutes: Routes = [
+    { path: "home", component: HomeComponent },
+];
